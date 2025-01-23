0812e16117748a5fab413d401fbad193684435db
Karthika <karthu0301@gmail.com>
2025-01-23 18:07:50 +0800

    A-TextUiTesting

--- src/main/java/Max.java
+++ src/main/java/Max.java
@@ -6,19 +6,23 @@ public class Max {
         Scanner scanner = new Scanner(System.in);
         Task[] tasks = new Task[100];
         int i = 0;
-        System.out.println("Hello I'm Max!\n It's so nice to meet you!!\n ");
+        System.out.println("Hello I'm Max!\n It's so nice to meet you!!\n");
 
         while (true) {
+            if (!scanner.hasNextLine()) {
+                break;
+            }
             String input = scanner.nextLine();
+
             String[] inputParts = input.split(" ", 2);
             if (input.equals("bye")) {
                 System.out.println("Bye! Will I see you again? :)");
                 break;
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (i = 0; i < tasks.length; i++) {
-                    if (tasks[i] != null) {
-                        System.out.print((i + 1) + ". " + tasks[i].toString() + "\n");
+                for (int j = 0; j < tasks.length; j++) {
+                    if (tasks[j] != null) {
+                        System.out.print(" "+ (j + 1) + ". " + tasks[j].toString() + "\n");
                     }
                 }
             } else if (inputParts[0].equals("mark") && inputParts.length == 2) {
@@ -28,25 +32,37 @@ public class Max {
                 int index = Integer.parseInt(inputParts[1]) - 1;
                 tasks[index].markAsNotDone();
             } else if (inputParts[0].equalsIgnoreCase("todo") && inputParts.length >= 2){
+                if (i >= tasks.length) {
+                    System.out.println("Task limit reached. Cannot add more tasks.");
+                    continue;
+                }
                 Task task = new ToDo(inputParts[1]);
                 tasks[i] = task;
                 i++;
-                System.out.println("Got it. I've added this task:\n" + task.toString() + "\n" +
+                System.out.println("Got it. I've added this task:\n  " + task.toString() + "\n" +
                         "Now you have " + (i) + " tasks in the list.");
             } else if (inputParts[0].equalsIgnoreCase("deadline") && inputParts.length >= 2){
+                if (i >= tasks.length) {
+                    System.out.println("Task limit reached. Cannot add more tasks.");
+                    continue;
+                }
                 String[] finalInputParts = inputParts[1].split("/by", 2);
                 Task task = new Deadline(finalInputParts[0],finalInputParts[1]);
                 tasks[i] = task;
                 i++;
-                System.out.println("Got it. I've added this task:\n" + task.toString() + "\n" +
+                System.out.println("Got it. I've added this task:\n  " + task.toString() + "\n" +
                         "Now you have " + (i) + " tasks in the list.");
             } else if (inputParts[0].equalsIgnoreCase("event") && inputParts.length >= 2){
+                if (i >= tasks.length) {
+                    System.out.println("Task limit reached. Cannot add more tasks.");
+                    continue;
+                }
                 String[] newInputParts = inputParts[1].split("/from", 2);
                 String[] finalInputParts = newInputParts[1].split("/to", 2);
                 Task task = new Event(newInputParts[0], finalInputParts[0],finalInputParts[1]);
                 tasks[i] = task;
                 i++;
-                System.out.println("Got it. I've added this task:\n" + task.toString() + "\n" +
+                System.out.println("Got it. I've added this task:\n  " + task.toString() + "\n" +
                         "Now you have " + (i) + " tasks in the list.");
             }
         }
