5eca9e6abbbc5529b38352bbb59feb64ff3b45b5
Karthika <karthu0301@gmail.com>
2025-01-23 14:48:53 +0800

    Level-1: Echo

--- src/main/java/Max.java
+++ src/main/java/Max.java
@@ -4,13 +4,13 @@ public class Max {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello I'm Max!\n What can I do for you?\n ");
+        System.out.println("Hello I'm Max!\n It's so nice to meet you!!\n ");
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
             System.out.println(input);
             input = scanner.nextLine();
         }
-            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("Bye. Will I see you soon? :)");
     }
 }
