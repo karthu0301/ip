b76e2396a9bea03cfe3e01ff20a18dc463f28cff
Karthika <karthu0301@gmail.com>
2025-01-23 16:12:22 +0800

    Level 3: Mark as Done and Not Done

--- src/main/java/Max.java
+++ src/main/java/Max.java
@@ -1,29 +1,38 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Max {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] listArray = new String[100];
-        int i = 1;
+        List<Task> taskList = new ArrayList<>();
         System.out.println("Hello I'm Max!\n It's so nice to meet you!!\n ");
-        String input = scanner.nextLine();
 
         while (true) {
+            String input = scanner.nextLine();
+            String[] inputParts = input.split(" ");
             if (input.equals("bye")) {
                 System.out.println("Bye! Will I see you again? :)");
                 break;
             } else if (input.equals("list")) {
-                for (String task : listArray) {
-                    if (task != null) {
-                        System.out.println(task);
-                    }
+                System.out.println("Here are the tasks in your list:");
+                int i = 1;
+                for (Task task : taskList) {
+                    System.out.print(i + ". ");
+                    task.displayTask();
+                    i++;
                 }
+            } else if (inputParts[0].equals("mark") && inputParts.length == 2) {
+                Task task = taskList.get(Integer.parseInt(inputParts[1]) - 1);
+                task.markAsDone();
+            } else if (inputParts[0].equals("unmark") && inputParts.length == 2) {
+                Task task = taskList.get(Integer.parseInt(inputParts[1]) - 1);
+                task.markAsNotDone();
             } else {
                 System.out.println("added: " + input);
-                listArray[i - 1] = i + "." + input;
-                i++;
+                taskList.add(new Task(input));
             }
-            input = scanner.nextLine();
         }
 
     }
