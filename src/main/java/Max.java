15193979592d028dadee1ccb91d2d72b2e3cfb58
Karthika <karthu0301@gmail.com>
2025-01-23 15:04:38 +0800

    Level 2: Add, List

--- src/main/java/Max.java
+++ src/main/java/Max.java
@@ -1,16 +1,31 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Max {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String[] listArray = new String[100];
+        int i = 1;
         System.out.println("Hello I'm Max!\n It's so nice to meet you!!\n ");
         String input = scanner.nextLine();
 
-        while (!input.equals("bye")) {
-            System.out.println(input);
+        while (true) {
+            if (input.equals("bye")) {
+                System.out.println("Bye! Will I see you again? :)");
+                break;
+            } else if (input.equals("list")) {
+                for (String task : listArray) {
+                    if (task != null) {
+                        System.out.println(task);
+                    }
+                }
+            } else {
+                System.out.println("added: " + input);
+                listArray[i - 1] = i + "." + input;
+                i++;
+            }
             input = scanner.nextLine();
         }
-            System.out.println("Bye. Will I see you soon? :)");
+
     }
 }
