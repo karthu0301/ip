7c227e154589ad2cf27406a59f102a5073ab3ce6
Karthika <karthu0301@gmail.com>
2025-02-03 08:47:06 +0800

    Level-9

--- src/main/java/max/parser/Parser.java
+++ src/main/java/max/parser/Parser.java
@@ -1,80 +1,50 @@
 package max.parser;
+
 import max.command.*;
 import max.exception.MaxException;
 
+/**
+ * Parses user input and returns the corresponding command.
+ */
 public class Parser {
-
+    /**
+     * Parses the user command and returns the appropriate Command object.
+     *
+     * @param fullCommand The full user input.
+     * @return The corresponding Command object.
+     * @throws MaxException If the command is invalid.
+     */
     public static Command parse(String fullCommand) throws MaxException {
         String[] inputParts = fullCommand.split(" ", 2);
         String commandWord = inputParts[0];
-        String arguments = inputParts.length > 1 ? inputParts[1].trim() : "";
+        String arguments = inputParts.length > 1 ? inputParts[1] : "";
 
         switch (commandWord.toLowerCase()) {
-            case "list":
-                return new ListCommand();
-
-            case "todo":
-                if (arguments.isEmpty()) {
-                    throw new MaxException("The description of a todo cannot be empty.");
-                }
-                return new AddCommand("todo", arguments);
-
-            case "deadline":
-                if (!arguments.contains("/by")) {
-                    throw new MaxException("Invalid deadline format! Use: deadline <description> /by <date-time>");
-                }
-                return new AddCommand("deadline", arguments);
-
-            case "event":
-                if (!arguments.contains("/from") || !arguments.contains("/to")) {
-                    throw new MaxException("Invalid event format! Use: event <description> /from <start> /to <end>");
-                }
-                return new AddCommand("event", arguments);
-
-            case "mark":
-                if (arguments.isEmpty()) {
-                    throw new MaxException("Please specify the task number to mark.");
-                }
-                try {
-                    int index = Integer.parseInt(arguments);
-                    return new MarkCommand(true, index);
-                } catch (NumberFormatException e) {
-                    throw new MaxException("Invalid task number for mark command.");
-                }
-
-            case "unmark":
-                if (arguments.isEmpty()) {
-                    throw new MaxException("Please specify the task number to unmark.");
-                }
-                try {
-                    int index = Integer.parseInt(arguments);
-                    return new MarkCommand(false, index);
-                } catch (NumberFormatException e) {
-                    throw new MaxException("Invalid task number for unmark command.");
-                }
-
-            case "delete":
-                if (arguments.isEmpty()) {
-                    throw new MaxException("Please specify the task number to delete.");
-                }
-                try {
-                    int index = Integer.parseInt(arguments);
-                    return new DeleteCommand(index);
-                } catch (NumberFormatException e) {
-                    throw new MaxException("Invalid task number for delete command.");
-                }
-
-            case "on":
-                if (arguments.isEmpty()) {
-                    throw new MaxException("Please specify a date to filter tasks.");
-                }
-                return new ShowCommand(arguments);
-
-            case "bye":
-                return new ExitCommand();
-
-            default:
-                throw new MaxException("Unknown command: " + commandWord);
+        case "list":
+            return new ListCommand();
+        case "todo":
+            return new AddCommand("todo", arguments);
+        case "deadline":
+            return new AddCommand("deadline", arguments);
+        case "event":
+            return new AddCommand("event", arguments);
+        case "mark":
+            return new MarkCommand(true, Integer.parseInt(arguments));
+        case "unmark":
+            return new MarkCommand(false, Integer.parseInt(arguments));
+        case "delete":
+            return new DeleteCommand(Integer.parseInt(arguments));
+        case "on":
+            return new ShowCommand(arguments);
+        case "find":
+            if (arguments.trim().isEmpty()) {
+                throw new MaxException("Please provide a keyword to search.");
+            }
+            return new FindCommand(arguments);
+        case "bye":
+            return new ExitCommand();
+        default:
+            throw new MaxException("Unknown command! Did you mean 'todo', 'deadline', 'event', or 'find'?");
         }
     }
 }
